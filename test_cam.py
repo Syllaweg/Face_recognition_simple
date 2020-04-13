3a490d9e3be5511a3f4ba7a8a321033e3fc76074
SylA68 <sylvere68@hotmail.fr>
2020-04-14 00:35:56 +0200

    add comment

--- test_cam.py
+++ test_cam.py
@@ -1,21 +1,23 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+# param (0) pour la webcam local
+video = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
 
-    # Our operations on the frame come here
+    ret, frame = video.read()
+
+    #  filtre gris sur la video
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
    
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
+    # active la webcam
     cv2.imshow('gray',gray)
+
+    # appuyer sur 'q' pour quitter la webcam
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
-cap.release()
+# ferme tout
+video.release()
 cv2.destroyAllWindows()
